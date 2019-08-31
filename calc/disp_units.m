810db0328030edae251198fa6a3feb4839e4a332
daniw <dani.winz@bluewin.ch>
2019-08-31 06:45:13 +0200

    disp_units to display values with SI unit prefix

---
+++ calc/disp_units.m
@@ -0,0 +1,38 @@
+% =============================================================================
+%> @brief Returns value as string to be used with SI units
+%> 
+%> @param  value  Value to be converted
+%> @param  unit   Unit to be appended after prefix
+%> @retval result Value as string with prefix and SI unit
+% =============================================================================
+function result = disp_units(value, unit)
+    if nargin == 1
+        unit = '';
+    end;
+    if isinf(value)
+        result = ['inf' unit];
+        warning('infinite number converted');
+    elseif value > 1e12
+        result = [num2str(value/1e12) 'T' unit];
+    elseif value > 1e9
+        result = [num2str(value/1e9) 'G' unit];
+    elseif value > 1e6
+        result = [num2str(value/1e6) 'M' unit];
+    elseif value > 1e3
+        result = [num2str(value/1e3) 'k' unit];
+    elseif value > 1e0
+        result = [num2str(value/1e0) '' unit];
+    elseif value > 1e-3
+        result = [num2str(value*1e3) 'm' unit];
+    elseif value > 1e-6
+        result = [num2str(value*1e6) 'u' unit];
+    elseif value > 1e-9
+        result = [num2str(value*1e9) 'n' unit];
+    elseif value > 1e-12
+        result = [num2str(value*1e12) 'p' unit];
+    elseif value > 1e-15
+        result = [num2str(value*1e15) 'f' unit];
+    else
+        result = [num2str(value*1e18) 'a' unit];
+    end;
+end
--- calc/max31740.m
+++ calc/max31740.m
@@ -56,46 +56,21 @@ temp_max_e = interp1(ntc_data_r, ntc_data_temp, r_ntc_max_e, 'pchip');
 disp(['Component values: ']);
 disp(['================= ']);
 % C_f1
-if c_f1_e > 1e-6
-    disp(['C_f1: ' num2str(c_f1_e*1e6) 'uF']);
-elseif c_f1_e > 1e-9
-    disp(['C_f1: ' num2str(c_f1_e*1e9) 'nF']);
-else
-    disp(['C_f1: ' num2str(c_f1_e*1e12) 'pF']);
-end;
-% C_f2
-if c_f2_e > 1e-6
-    disp(['C_f2: ' num2str(c_f2_e*1e6) 'uF']);
-elseif c_f2_e > 1e-9
-    disp(['C_f2: ' num2str(c_f2_e*1e9) 'nF']);
-else
-    disp(['C_f2: ' num2str(c_f2_e*1e12) 'pF']);
-end;
+disp(['C_f1:    ' disp_units(c_f1_e, 'F')]);
+disp(['C_f2:    ' disp_units(c_f2_e, 'F')]);
 % R_st
-if r_st_e > 1e6
-    disp(['R_st: ' num2str(r_st_e/1e6) 'MOhm']);
-elseif r_st_e > 1e3
-    disp(['R_st: ' num2str(r_st_e/1e3) 'kOhm']);
-else
-    disp(['R_st: ' num2str(r_st_e/1e0) 'Ohm']);
-end;
+disp(['R_st:    ' disp_units(r_st_e, 'Ohm')]);
 % R_slope
-if r_slope_e > 1e6
-    disp(['R_slope: ' num2str(r_slope_e/1e6) 'MOhm']);
-elseif r_slope_e > 1e3
-    disp(['R_slope: ' num2str(r_slope_e/1e3) 'kOhm']);
-else
-    disp(['R_slope: ' num2str(r_slope_e/1e0) 'Ohm']);
-end;
+disp(['R_slope: ' disp_units(r_slope_e, 'Ohm')]);
 
 %% Display resulting operating parameters
 disp([' ']);
 disp(['Operating parameters: ']);
 disp(['===================== ']);
-disp(['Low switching frequency:  ' num2str(f2_e) 'Hz']);
-disp(['High switching frequency: ' num2str(f1_e/1000) 'kHz']);
-disp(['Starting temperature:     ' num2str(temp_start_e) '°C']);
-disp(['Max speed temperature:    ' num2str(temp_max_e) '°C']);
+disp(['Low switching frequency:  ' disp_units(f2_e, 'Hz')]);
+disp(['High switching frequency: ' disp_units(f1_e, 'Hz')]);
+disp(['Starting temperature:     ' disp_units(temp_start_e, '°C')]);
+disp(['Max speed temperature:    ' disp_units(temp_max_e, '°C')]);
 
 %% Plot duty as function of temperature
 t = floor(temp_start_e)-2:0.01:ceil(temp_max_e)+2;
