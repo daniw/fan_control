a46ee8c3cebc0496c6cf025fcd5b825f80d6884e
daniw <dani.winz@bluewin.ch>
2019-09-19 07:03:57 +0200

    Spacer added as optional parameter to disp_units

--- calc/disp_units.m
+++ calc/disp_units.m
@@ -5,46 +5,51 @@
 %> @param  unit   Unit to be appended after prefix
 %> @retval result Value as string with prefix and SI unit
 % =============================================================================
-function result = disp_units(value, unit)
+function result = disp_units(value, unit, spacer)
     if nargin == 1
         unit = '';
+    end;
+    if nargin <= 2
+        spacer = ' ';
     end;
     if isinf(value)
         result = ['inf' unit];
         warning('infinite number converted');
+    elseif value == 0
+        result = [num2str(value) unit];
     elseif value > 1e24
-        result = [num2str(value/1e24) 'Y' unit];
+        result = [num2str(value/1e24) spacer 'Y' unit];
     elseif value > 1e21
-        result = [num2str(value/1e21) 'Z' unit];
+        result = [num2str(value/1e21) spacer 'Z' unit];
     elseif value > 1e18
-        result = [num2str(value/1e18) 'E' unit];
+        result = [num2str(value/1e18) spacer 'E' unit];
     elseif value > 1e15
-        result = [num2str(value/1e15) 'P' unit];
+        result = [num2str(value/1e15) spacer 'P' unit];
     elseif value > 1e12
-        result = [num2str(value/1e12) 'T' unit];
+        result = [num2str(value/1e12) spacer 'T' unit];
     elseif value > 1e9
-        result = [num2str(value/1e9) 'G' unit];
+        result = [num2str(value/1e9)  spacer 'G' unit];
     elseif value > 1e6
-        result = [num2str(value/1e6) 'M' unit];
+        result = [num2str(value/1e6)  spacer 'M' unit];
     elseif value > 1e3
-        result = [num2str(value/1e3) 'k' unit];
+        result = [num2str(value/1e3)  spacer 'k' unit];
     elseif value > 1e0
-        result = [num2str(value/1e0) '' unit];
+        result = [num2str(value/1e0)  spacer ''  unit];
     elseif value > 1e-3
-        result = [num2str(value*1e3) 'm' unit];
+        result = [num2str(value*1e3)  spacer 'm' unit];
     elseif value > 1e-6
-        result = [num2str(value*1e6) 'u' unit];
+        result = [num2str(value*1e6)  spacer 'u' unit];
     elseif value > 1e-9
-        result = [num2str(value*1e9) 'n' unit];
+        result = [num2str(value*1e9)  spacer 'n' unit];
     elseif value > 1e-12
-        result = [num2str(value*1e12) 'p' unit];
+        result = [num2str(value*1e12) spacer 'p' unit];
     elseif value > 1e-15
-        result = [num2str(value*1e15) 'f' unit];
+        result = [num2str(value*1e15) spacer 'f' unit];
     elseif value > 1e-18
-        result = [num2str(value*1e18) 'a' unit];
+        result = [num2str(value*1e18) spacer 'a' unit];
     elseif value > 1e-21
-        result = [num2str(value*1e21) 'z' unit];
+        result = [num2str(value*1e21) spacer 'z' unit];
     else
-        result = [num2str(value*1e24) 'y' unit];
+        result = [num2str(value*1e24) spacer 'y' unit];
     end;
 end
